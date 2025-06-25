f4ac58d76a1b76d8ac12e6f91efcc25e2969f867
unknown <SENA@PALDFPCBIFSD030.FormacionSena.RED>
2025-06-24 21:26:47 -0500

    Update_dateBase24062025

--- NewProyectoBikeStore/Database/scriptDataBaseBikeStore.sql
+++ NewProyectoBikeStore/Database/scriptDataBaseBikeStore.sql
@@ -1,72 +1,3 @@
-/*CREATE DATABASE bike_store;
-USE bike_store;
-
--- Tabla: productos
-CREATE TABLE productos (
-    id_producto INT AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(255) NOT NULL,
-    precio_venta DECIMAL(10,2) NOT NULL CHECK (precio_venta >= 0),
-    descripcion VARCHAR(500),
-    destacado BOOLEAN DEFAULT FALSE,
-    imagen VARCHAR(255)
-);
-
--- Tabla: stocks (1:1 con productos)
-CREATE TABLE stocks (
-    id_stock INT AUTO_INCREMENT PRIMARY KEY,
-    stock INT CHECK (stock >= 0),
-	stock_minimo INT DEFAULT 0 CHECK (stock_minimo >= 0),
-    estado ENUM('disponible', 'agotado') DEFAULT 'disponible',
-    fecha_actualizacion DATETIME DEFAULT CURRENT_TIMESTAMP,
-    id_producto INT UNIQUE,
-    FOREIGN KEY (id_producto) REFERENCES productos(id_producto) ON DELETE CASCADE
-);
-
--- Tabla: usuarios
-CREATE TABLE usuarios (
-    id_usuario INT AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(100) NOT NULL,
-    apellido VARCHAR(100) NOT NULL,
-    telefono VARCHAR(20) UNIQUE NOT NULL,
-    direccion VARCHAR(100) NOT NULL,
-    correo VARCHAR(100) UNIQUE NOT NULL,
-    clave VARCHAR(255) NOT NULL,
-    rol ENUM('cliente', 'admin', 'super_usuario') DEFAULT 'cliente',
-    fecha_registro DATETIME DEFAULT CURRENT_TIMESTAMP
-);
-
--- Tabla: ventas
-CREATE TABLE ventas (
-    id_venta INT AUTO_INCREMENT PRIMARY KEY,
-	cantidad_productos INT NOT NULL CHECK (cantidad_productos > 0),
-    precio_productos DECIMAL(10,2) NOT NULL CHECK (precio_productos >= 0),
-    metodo_pago ENUM('visa', 'mastercard', 'maestro', 'paypal') NOT NULL,
-    numero_tarjeta VARCHAR(50),
-    fecha_venta DATETIME DEFAULT CURRENT_TIMESTAMP,
-    id_usuario INT,
-    FOREIGN KEY (id_usuario) REFERENCES usuarios(id_usuario)
-);
-
--- Tabla: detalles_venta (relación N:M entre productos y ventas)
-CREATE TABLE detalles_venta (
-    id_detalle_venta INT AUTO_INCREMENT PRIMARY KEY,
-    cantidad_producto INT NOT NULL CHECK (cantidad_producto > 0),
-    precio_unitario DECIMAL(10,2) NOT NULL CHECK (precio_unitario >= 0),
-    id_producto INT,
-    id_venta INT,
-    FOREIGN KEY (id_producto) REFERENCES productos(id_producto),
-    FOREIGN KEY (id_venta) REFERENCES ventas(id_venta)
-);
-
-/Consultas*/
-/*UPDATE usuarios
-SET rol = 'admin'
-WHERE id_usuario = 1; /
-
-Select * From usuarios;*/
-
-
--- Modificacion de la base de datos para incluir la tabla producto_detVenta
 CREATE DATABASE bike_store;
 USE bike_store;
 
@@ -76,19 +7,10 @@ CREATE TABLE productos (
     nombre VARCHAR(255) NOT NULL,
     precio_venta DECIMAL(10,2) NOT NULL CHECK (precio_venta >= 0),
     descripcion VARCHAR(500),
-    destacado BOOLEAN DEFAULT FALSE,
-    imagen VARCHAR(255)
-);
-
--- Tabla: stocks (1:1 con productos)
-CREATE TABLE stocks (
-    id_stock INT AUTO_INCREMENT PRIMARY KEY,
-    stock INT CHECK (stock >= 0),
-	stock_minimo INT DEFAULT 0 CHECK (stock_minimo >= 0),
-    estado ENUM('disponible', 'agotado') DEFAULT 'disponible',
-    fecha_actualizacion DATETIME DEFAULT CURRENT_TIMESTAMP,
-    id_producto INT UNIQUE,
-    FOREIGN KEY (id_producto) REFERENCES productos(id_producto) ON DELETE CASCADE
+    imagen VARCHAR(255),
+    entrada INT,
+    salida INT,
+    saldo INT GENERATED ALWAYS AS (entrada - salida) STORED
 );
 
 -- Tabla: usuarios
@@ -104,30 +26,22 @@ CREATE TABLE usuarios (
     fecha_registro DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
--- Tabla: ventas
 CREATE TABLE ventas (
     id_venta INT AUTO_INCREMENT PRIMARY KEY,
-	cantidad_productos INT NOT NULL CHECK (cantidad_productos > 0),
-    precio_productos DECIMAL(10,2) NOT NULL CHECK (precio_productos >= 0),
-    metodo_pago ENUM('visa', 'mastercard', 'maestro', 'paypal') NOT NULL,
-    numero_tarjeta VARCHAR(50),
+    id_usuario INT NOT NULL,
     fecha_venta DATETIME DEFAULT CURRENT_TIMESTAMP,
-    id_usuario INT,
+    metodo_pago ENUM('visa', 'mastercard', 'paypal') NOT NULL,
+    estado ENUM('Pendiente', 'Despachado', 'Concluido') DEFAULT 'Pendiente',
     FOREIGN KEY (id_usuario) REFERENCES usuarios(id_usuario)
 );
 
-/*  Tabla producto/det_venta la cual rompera la relación de N:M entre producto y 
-det_venta ya que un producto esta asociado a muchas ventas, pero una venta va a contener muchos productos   */
 CREATE TABLE detalles_venta (
-	id_producto INT,
-    id_venta INT,
-    FOREIGN KEY (id_producto) REFERENCES productos(id_producto),
-    FOREIGN KEY (id_venta) REFERENCES ventaS(id_venta)
+    id_detalle INT AUTO_INCREMENT PRIMARY KEY,
+    id_venta INT NOT NULL,
+    id_producto INT NOT NULL,
+    cantidad INT NOT NULL,
+    precio_unitario DECIMAL(10,2) NOT NULL,
+    total DECIMAL(10,2) GENERATED ALWAYS AS (cantidad * precio_unitario) STORED,
+    FOREIGN KEY (id_venta) REFERENCES ventas(id_venta),
+    FOREIGN KEY (id_producto) REFERENCES productos(id_producto)
 );
-
-/*Consultas*/
-/*UPDATE usuarios
-SET rol = 'admin'
-WHERE id_usuario = 1;*/
-
-Select * From usuarios;
